6aa5f102255e6b5e99a0e0221b21cb2ddda102d0
sfog17 <>
2021-04-18 17:43:59 +0100

    handle edge cases + logging

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 """ Inspired by https://towardsdatascience.com/creating-interactive-maps-for-instagram-with-python-and-folium-68bc4691d075 """
 
 import folium
+import logging
 from pathlib import Path
 from typing import Any, Dict, Tuple
 from PIL import Image
@@ -8,9 +9,9 @@ from PIL.ExifTags import TAGS, GPSTAGS
 
 
 def resize_pictures(dir_in: Path, dir_out: Path, height: int, width: int):
-    for path_img_in in list(dir_in.glob("*.jpg")):
+    for path_img_in in list(dir_in.rglob("*.jpg")):
         path_img_out = Path(dir_out / (path_img_in.name))
-        print(f'Converting {path_img_in} to {path_img_out} - Size {height} x {width}')
+        logging.info(f'Converting {path_img_in} to {path_img_out} - Size {height} x {width}')
         with Image.open(path_img_in) as img:
             img.thumbnail((height, width), Image.ANTIALIAS)
             img.save(path_img_out, exif=img.getexif())
@@ -27,12 +28,15 @@ def convert_angle(angle: Tuple[float, float, float], ref: str) -> float:
 def get_exif_readable(path_img_in: Path) -> Dict[str, Any]:
     image = Image.open(path_img_in)
     exif = image._getexif()
-    exif_readable = {TAGS[k]: v for k,v in exif.items()}
+    exif_readable = {TAGS[k]: v for k,v in exif.items() if k in TAGS}
+    if 'GPSInfo' in exif_readable:
+        gps_info = {GPSTAGS[k]: v for k,v in exif_readable['GPSInfo'].items()}
+        exif_readable['GPSInfo'] = gps_info
     return exif_readable
 
 
 def get_lat_lon(exif_readable: Dict[str, Any]) -> Tuple[float, float]:
-    gps_info = {GPSTAGS[k]: v for k,v in exif_readable['GPSInfo'].items()}
+    gps_info = exif_readable['GPSInfo']
     latitude = convert_angle(gps_info['GPSLatitude'], gps_info['GPSLatitudeRef'])
     longitude = convert_angle(gps_info['GPSLongitude'], gps_info['GPSLongitudeRef'])
     return (latitude, longitude)
@@ -47,29 +51,31 @@ def plot_map(dir_photos: Path):
     # Add Marker for each photo
     for path_img_in in list(dir_photos.glob("*.jpg")):
         exif = get_exif_readable(path_img_in)
-        # Re-orient picture
-        orient = exif['Orientation']
-        if orient == 1:
-            width, height = 640, 480
-        elif orient == 6:
-            width, height = 480, 640
-        else:
-            raise ValueError('Orientation value in exif data is not 1 or 6')
-        # Add GPS location marker
-        if exif['GPSInfo']:
+        try:
+            # Re-orient picture
+            orient = exif['Orientation']
+            if orient in (0, 1, 3):
+                width, height = 640, 480
+            elif orient in (6, 8):
+                width, height = 480, 640
+            else:
+                raise ValueError(f'Orientation value in exif data is not handled - {path_img_in} - {exif}')
+            # Add GPS location marker
             folium.Marker(
                 location=list(get_lat_lon(exif)),
                 popup=f'<img src="{path_img_in.as_posix()}" width="{width}" height="{height}">',
                 icon=folium.Icon(icon="paw", prefix='fa', color='red')
             ).add_to(my_map)
-        else:
-            print(f'Picture {path_img_in} has no GPS information')
+
+        except KeyError as err:
+            logging.warning(f'Key {err} missing - Picture {path_img_in}')
 
     # Save to HTML
     my_map.save("index.html")
 
 
 def main():
+    logging.basicConfig(level=logging.ERROR)
     dir_original = Path('photos/original')
     dir_thumbnail = Path('photos/thumbnail')
     resize_pictures(dir_original, dir_thumbnail, 640, 480)
